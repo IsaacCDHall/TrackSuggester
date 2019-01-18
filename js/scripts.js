9ab07c5d069db776e4dbd65b648f4e8262b53dfe
Isaac Hall <isaacdehall@gmail.com>
2019-01-18 10:20:11 -0800

    add global functions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 
+function cssDesign(question1, question2, question3, question4, question5) {
+  if((question1 + question2 + question3 + question4 + question5) <= 5 ) {
+    return true
+  } else {
+    return false
+  }
+}
+function javaRuby(question1, question2, question3, question4, question5) {
+  if((question1 + question2 + question3 + question4 + question5) <= 10 ) {
+    return true
+  } else {
+    return false
+  }
+}
+function csharpPhp(question1, question2, question3, question4, question5) {
+  if((question1 + question2 + question3 + question4 + question5) <= 15 ) {
+    return true
+  } else {
+    return false
+  }
+}
+console.log("yeeeeee");
